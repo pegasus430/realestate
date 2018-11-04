630f02dfe165516da29d68311c54457a1fb31dc0
softcomet <realseniordev210@gmail.com>
2018-11-04 06:48:54 -0500

    modify bienici

--- realestate/spiders/bienici.py
+++ realestate/spiders/bienici.py
@@ -101,8 +101,8 @@ class selogerSpider(Spider):
             next_page_url = 'https://www.bienici.com/realEstateAds.json?filters=%7B%22size%22%3A24%2C%22from%22%3A{}%2C%22filterType%22%3A%22rent%22%2C%22propertyType%22%3A%5B%22house%22%2C%22flat%22%5D%2C%22page%22%3A{}%2C%22resultsPerPage%22%3A24%2C%22maxAuthorizedResults%22%3A2400%2C%22sortBy%22%3A%22relevance%22%2C%22sortOrder%22%3A%22desc%22%2C%22onTheMarket%22%3A%5Btrue%5D%2C%22showAllModels%22%3Afalse%2C%22zoneIdsByTypes%22%3A%7B%22zoneIds%22%3A%5B%22-7444%22%5D%7D%7D&extensionType=extendedIfNoResult&leadingCount=2'.format(next, page)
             yield Request(next_page_url, self.parse)
 
-        # if next_page_url:
-        #     yield Request(response.urljoin(next_page_url), callback=self.parse, dont_filter=True)
+        if next_page_url:
+            yield Request(response.urljoin(next_page_url), callback=self.parse, dont_filter=True)
 
 
     def final_parse(self, response):
