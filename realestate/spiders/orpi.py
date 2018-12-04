a7dd14eaebff4f871a4ffaa05ded15a30137ce17
softcomet <realseniordev210@gmail.com>
2018-12-04 08:21:03 -0500

    modify orpi

--- realestate/spiders/orpi.py
+++ realestate/spiders/orpi.py
@@ -26,32 +26,32 @@ class selogerSpider(Spider):
 
     # sys.setdefaultencoding('utf-8')
 
-   # //////// angel headers and cookies////////////
+    # //////// angel headers and cookies////////////
     # --------------- Get list of proxy-----------------------#
-    # proxy_text = requests.get('https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt').text
-    # list_proxy_temp = proxy_text.split('\n')
-    # list_proxy = []
-    # for line in list_proxy_temp:
-    #     if line.strip() !='' and (line.strip()[-1] == '+' or line.strip()[-1] == '-'):
-    #         ip = line.strip().split(':')[0].replace(' ', '')
-    #         port = line.split(':')[-1].split(' ')[0]
-    #         list_proxy.append('http://'+ip+':'+port)
+    proxy_text = requests.get('https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt').text
+    list_proxy_temp = proxy_text.split('\n')
+    list_proxy = []
+    for line in list_proxy_temp:
+        if line.strip() !='' and (line.strip()[-1] == '+' or line.strip()[-1] == '-'):
+            ip = line.strip().split(':')[0].replace(' ', '')
+            port = line.split(':')[-1].split(' ')[0]
+            list_proxy.append('http://'+ip+':'+port)
 
     def start_requests(self):
         # proxy = random.choice(self.list_proxy)
         yield Request(self.start_url, callback= self.parse, meta={"next_count": 1})
 
-    # def errCall(self, response):
-    #     ban_proxy = response.request.meta['proxy']
-    #     self.list_proxy.remove(ban_proxy)
-    #     proxy = random.choice(self.list_proxy)
-    #     # response.request.meta['proxy'] = proxy
-    #     print ('err proxy: ' + proxy)
-    #     yield Request(response.request.url,
-    #                   callback=self.parse,
-    #                   meta={'proxy': proxy},
-    #                   dont_filter=True,
-    #                   errback=self.errCall)
+    def errCall(self, response):
+        ban_proxy = response.request.meta['proxy']
+        self.list_proxy.remove(ban_proxy)
+        proxy = random.choice(self.list_proxy)
+        # response.request.meta['proxy'] = proxy
+        print ('err proxy: ' + proxy)
+        yield Request(response.request.url,
+                      callback=self.parse,
+                      meta={'proxy': proxy},
+                      dont_filter=True,
+                      errback=self.errCall)
 
     def parse(self, response):
         dataInfos = json.loads(response.text.split('Orpi.saveResultsInSession(')[-1].split('], ')[0] + ']')
@@ -63,10 +63,10 @@ class selogerSpider(Spider):
             # yield Request(response.urljoin(urls[0]), callback= self.final_parse, dont_filter=True)
             ###################################################################################
 
-            # main_url = 'https://www.orpi.com/recherche/rent?realEstateTypes%5B%5D=maison&realEstateTypes%5B1%5D=appartement&locations%5B0%5D=paris&sort=date-down&layoutType=mixte&page={}'
-            # next_count = response.meta["next_count"] + 1
-            # next_page_url = main_url.format(str(next_count))
-            # yield Request(response.urljoin(next_page_url), callback=self.parse, dont_filter=True, meta={"next_count": next_count})
+            main_url = 'https://www.orpi.com/recherche/rent?realEstateTypes%5B%5D=maison&realEstateTypes%5B1%5D=appartement&locations%5B0%5D=paris&sort=date-down&layoutType=mixte&page={}'
+            next_count = response.meta["next_count"] + 1
+            next_page_url = main_url.format(str(next_count))
+            yield Request(response.urljoin(next_page_url), callback=self.parse, dont_filter=True, meta={"next_count": next_count})
 
 
     def final_parse(self, response):
