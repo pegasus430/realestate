1ab421b9906c8c1132c0b5397e2ed4311fde203a
softcomet <realseniordev210@gmail.com>
2018-12-11 09:17:46 -0500

    modify omni

--- realestate/spiders/ommi.py
+++ realestate/spiders/ommi.py
@@ -51,16 +51,16 @@ class ommiSpider(Spider):
 
     sys.setdefaultencoding('utf-8')
 
-    headers = {
-                'Cache-Control': 'max-age=0',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9',
-                'upgrade-insecure-requests': '1',
-                'referer': 'https://www.tradecarview.com/my/favoritelist.aspx?list=1&sort=0&ps=25&&pn=0'
-            }
+    # headers = {
+    #             'Cache-Control': 'max-age=0',
+    #             'upgrade-insecure-requests': '1',
+    #             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    #             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    #             'accept-encoding': 'gzip, deflate, br',
+    #             'accept-language': 'en-US,en;q=0.9',
+    #             'upgrade-insecure-requests': '1',
+    #             'referer': 'https://www.tradecarview.com/my/favoritelist.aspx?list=1&sort=0&ps=25&&pn=0'
+    #         }
 
     proxy_text = requests.get('https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt').text
     list_proxy_temp = proxy_text.split('\n')
@@ -72,7 +72,7 @@ class ommiSpider(Spider):
             list_proxy.append('http://'+ip+':'+port)
 
     def start_requests(self):
-        # proxy = random.choice(self.list_proxy)
+        proxy = random.choice(self.list_proxy)
         yield Request(self.start_url, callback= self.parse, headers=self.headers)
 
     def errCall(self, response):
